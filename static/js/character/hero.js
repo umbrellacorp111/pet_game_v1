5d942e44c7d143b7f62920c9ced79416a060c5b2
umbrellacorp111 <163636061+umbrellacorp111@users.noreply.github.com>
2026-07-04 14:46:34 +0400

    Add files via upload

--- static/js/character/hero.js
+++ static/js/character/hero.js
@@ -183,7 +183,7 @@ window.Hero = (() => {
         loader.load(url, fbx => {
           // Three.js FBXLoader сам конвертит Z-up → Y-up.
           // Масштаб: ~14 единиц FBX → ~2 единицы сцены.
-          fbx.scale.setScalar(.85);
+          fbx.scale.setScalar(1.00);
 
           const mixer = new THREE.AnimationMixer(fbx);
           // Проигрываем первую анимацию на цикле, если есть
